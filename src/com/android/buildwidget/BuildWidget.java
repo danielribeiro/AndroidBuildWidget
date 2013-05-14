b7caff97fa073f569043d1906f6e5648f7331eb8
Daniel Ribeiro <danielrb@pagerduty.com>
2013-05-13 21:47:18 -0700

    massive simplification

--- src/com/android/buildwidget/BuildWidget.java
+++ src/com/android/buildwidget/BuildWidget.java
@@ -28,103 +28,44 @@ import android.text.format.DateUtils;
 import android.text.format.Time;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Define a simple widget that shows the Wiktionary "Word of the day." To build
- * an update we spawn a background {@link Service} to perform the API queries.
+ * Totally based of : http://stackoverflow.com/a/2748723
  */
 public class BuildWidget extends AppWidgetProvider {
+    public static String YOUR_AWESOME_ACTION = "YourAwesomeAction";
+
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-            int[] appWidgetIds) {
-        // To prevent any ANR timeouts, we perform the update in a service
-        context.startService(new Intent(context, UpdateService.class));
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.i("--> on update", "updating");
+        Log.i("--> on update", "size: "+ appWidgetIds.length);
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int appWidgetId : appWidgetIds) {
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, BuildWidget.class);
+            intent.setAction(YOUR_AWESOME_ACTION);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.text, pendingIntent);
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 
-    public static class UpdateService extends Service {
-        private static final String ACTION = "com.android.samples.updateWidget";
-        private int callledCount = 0;
-        private BroadcastReceiver yourReceiver;
 
-
-        private void setupService() {
-            final UpdateService self = this;
-            log("setting up service");
-
-            super.onCreate();
-            final IntentFilter theFilter = new IntentFilter();
-            theFilter.addAction(ACTION);
-            this.yourReceiver = new BroadcastReceiver() {
-
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    log("received intent");
-                    // Do whatever you need it to do when it receives the broadcast
-                    // Example show a Toast message...
-                    RemoteViews updateViews = new RemoteViews(
-                            context.getPackageName(), R.layout.widget);
-                    updateViews.setTextViewText(R.id.text, "update count" + callledCount);
-                    // Push update for this widget to the home screen
-                    ComponentName thisWidget = new ComponentName(self, BuildWidget.class);
-                    AppWidgetManager manager = AppWidgetManager.getInstance(self);
-                    manager.updateAppWidget(thisWidget, updateViews);
-                }
-
-
-            };
-            // Registers the receiver so that your service will listen for
-            // broadcasts
-            this.registerReceiver(this.yourReceiver, theFilter);
-
-        }
-
-        private void log(String s) {
-            Log.i("--> APP", s);
-        }
-
-        @Override
-        public void onDestroy() {
-            log("destroying");
-            super.onDestroy();
-            // Do not forget to unregister the receiver!!!
-            this.unregisterReceiver(this.yourReceiver);
-        }
-
-
-        @Override
-        public void onStart(Intent intent, int startId) {
-            log("on start");
-            setupService();
-            // Build the widget update
-            RemoteViews updateViews = buildUpdate(this);
-
-            // Push update for this widget to the home screen
-            ComponentName thisWidget = new ComponentName(this, BuildWidget.class);
-            AppWidgetManager manager = AppWidgetManager.getInstance(this);
-            manager.updateAppWidget(thisWidget, updateViews);
-        }
-
-        public RemoteViews buildUpdate(Context context) {
-            // Pick out month names from resources
-            Resources res = context.getResources();
-            RemoteViews updateViews = new RemoteViews(
-                context.getPackageName(), R.layout.widget);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(context,
-                    0 /* no requestCode */, 
-                    new Intent(ACTION),
-                    0 /* no flags */);
-            updateViews.setOnClickPendingIntent(R.id.text, pendingIntent);
-            return updateViews;
-        }
-
-        @Override
-        public IBinder onBind(Intent intent) {
-            // We don't need to bind to this service
-            return null;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.i("--> message received", intent.getAction());
+        if (intent.getAction().equals(YOUR_AWESOME_ACTION)) {
+            Toast.makeText(context, "Touched view ", Toast.LENGTH_SHORT).show();
         }
     }
 }
